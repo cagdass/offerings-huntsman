1a25af77a07787c80ef867c3c60223fe9f0b1291
Çağdaş <cagdas.oztekin@ug.bilkent.edu.tr>
2016-08-09 11:23:13 +0300

    Nicely comment

--- parser/parser.py
+++ parser/parser.py
@@ -17,9 +17,12 @@ class Parser:
     count = 0
     currentCourse = {}
 
+    # Keeping the count of how many td elements have been looked at so far.
     for td in self.td_list:
       try:
         data = td.text
+        # For each condition that follows, examples of the data inside will be given from CS425.
+        # Course information. Example: 'CS425-1'. Separated into departmentCode: 'CS', courseCode: '425', section: '1'.
         if count == 0:
           departmentCode = str(data.split(" ")[0])
           currentCourse['departmentCode'] = departmentCode
@@ -28,14 +31,31 @@ class Parser:
           section = data[4:]
           currentCourse['courseCode'] = str(courseCode)
           currentCourse['section'] = str(section)
+        # Course information. Example: 'Algorithms for Web-Scale Data'
         elif count == 1:
           currentCourse['title'] = str(data)
+        # Instructor(s). Example: 'Mustafa Ozdal'
         elif count == 2:
-          currentCourse['instructor'] = data
+          spans = td.find_all('span')
+          # Instructor is "Staff" if there are no <span>'s inside <td>
+          if len(spans) == 0:
+              currentCourse['instructor'] = [data]
+          else:
+              # Separate them if the course has multiple instructors.
+              # Initialize instructors array. Append new instructors from each span as you go.
+              instructors = []
+              for span in spans:
+                  data = span.text
+                  instructors.append(data)
+                #   print "Appending " + data
+              currentCourse['instructor'] = instructors
+        # Bilkent credits. Example: '3'
         elif count == 3:
           currentCourse['bilkentCredit'] = str(data)
+        # ECTS credits. Example: '6'
         elif count == 4:
           currentCourse['ectsCredit'] = str(data)
+        # An array of lecture hours/locations. lectures[0] could be {day: 'Tue', startHour: '15:40', endHour: '16:30', status: '', location: 'EA-Z03'}
         elif count == 5:
           lectures = []
           for line in data.split('\n'):
@@ -52,21 +72,28 @@ class Parser:
                   location = location[:-3]
               lectures.append({'day': day, 'hours': hours, 'location': location, 'status': status})
               currentCourse['lectures'] = lectures
+        # Total quota of the course. '65' is quite common.
         elif count == 7:
           currentCourse['totalQuota'] = str(data)
+        # Number of must enrollments. Equals to total quota if it is a popular course.
         elif count == 8:
           currentCourse['enrollmentMust'] = str(data)
+        # Number of elective enrollments. Get out of our CS classes y'all EE people.
         elif count == 9:
           currentCourse['enrollmentElective'] = str(data)
+        # Total number of enrollments, must equal to (must + elective) enrollments.
         elif count == 10:
           currentCourse['enrollmentTotal'] = str(data)
           try:
+              # Assert the (must + elective) = total equality. Put in try/except to avoid future bugs.
               assert int(currentCourse['enrollmentTotal']) == int(currentCourse['enrollmentMust']) + int(currentCourse['enrollmentElective'])
           except AssertionError:
               print "Problem with total == must + elective"
+        # Number of available quota. Usually '0' for popular courses.
         elif count == 12:
           currentCourse['availableQuota'] = str(data)
       except UnicodeEncodeError:
         pass
       count += 1
+
     return currentCourse
